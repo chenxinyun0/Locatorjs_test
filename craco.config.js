a1e0dd99f533e23707e792713808e04f9f25be8a
陈鑫宇 <xinyu.chen@eeoa.com>
2023-03-19 14:51:03 +0800

    fix: 尝试

---
+++ craco.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  babel: {
+    plugins: [
+      ["@locator/babel-jsx/dist", {
+        env: "development"
+      }]
+    ],
+  },
+};
---
+++ src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button, Modal } from 'antd';
+import { useState } from 'react';
+
+function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button type="primary" onClick={showModal}>
+          Open Modal
+        </Button>
+        <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+          <p>Some contents...1111111</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
+      </header>
+    </div>
+  );
+}
+
+export default App;
